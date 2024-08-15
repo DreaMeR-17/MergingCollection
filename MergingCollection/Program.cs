6882e55c57fa11f3e0f28747a713819df9d5f60b
DreaMeR-17 <pronka_1993@mail.ru>
2024-08-15 21:09:54 +0600

    RE:

--- MergingCollection/Program.cs
+++ MergingCollection/Program.cs
@@ -7,47 +7,52 @@ namespace MergingCollection
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
-
             int[] array1 = { 2, 4, 9, 11, 21, 7, 18, 5, 50 };
             int[] array2 = { 2, 3, 5, 16, 13, 7, 9, 18, 50 };
 
-            numbers.AddRange(array1);
-            numbers.AddRange(array2);
+            Console.Write("Первый массив: ");
+            ShowArray(array1);
 
-            Console.Write("Коллекция до изменения: ");
-            ShowArray(numbers);
+            Console.Write("Второй массив: ");
+            ShowArray(array2);
 
-            Console.Write($"\nИзмененная коллекция: ");
-            MergingCollection(numbers);
-            ShowArray(numbers);
+            List<int> numbers = new List<int>();
+
+            MergingCollection(numbers, array1);
+            MergingCollection(numbers, array2);
+
+            Console.Write("\nМассивы после объединения: ");
+            ShowList(numbers);
         }
 
-        static void ShowArray(List<int> numbers)
+        static void MergingCollection(List<int> numbers, int[] array)
         {
-            for (int i = 0; i < numbers.Count; i++)
+            foreach (var value in array)
+            {
+                if (numbers.Contains(value) == false)
+                {
+                    numbers.Add(value);
+                }
+            }
+        }
+
+        static void ShowList(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
             {
                 Console.Write($"{numbers[i]} ");
             }
+
+            Console.WriteLine(numbers[numbers.Count - 1]);
         }
 
-        static void MergingCollection(List<int> numbers)
+        static void ShowArray(int[] numbers)
         {
-            for (int i = 0; i < numbers.Count; i++)
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
-                for (int j = i + 1; j < numbers.Count; j++)
-                {
-                    if (numbers[i] == numbers[j])
-                    {
-                        numbers.RemoveAt(j);
-                    }
-                    else if (numbers[j] < numbers[i])
-                    {
-                        numbers.Insert(i, numbers[j]);
-                        numbers.RemoveAt(j + 1);
-                    }
-                }
+                Console.Write($"{numbers[i]} ");
             }
+            Console.WriteLine(numbers[numbers.Length - 1]);
         }
     }
 }
